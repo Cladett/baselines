91c0acae1ea3a30cf0ea95205de469ee045261ff
Clauda D'ettore <c.dettorre@ucl.ac.uk>
2021-07-05 07:19:28 +0100

    Pushing results of pick rail and pickplacetarget with demo for dVRL.

--- baselines/her/her.py
+++ baselines/her/her.py
@@ -134,7 +134,8 @@ def learn(*, network, env, total_timesteps,
     # Mod by Claudia D'Ettorre (19 Nov 2020): if there is no demo file there 
     # there is no behavioural cloning (bc_loss)
     params['bc_loss'] = 1 if demo_file is not None else 0
-    # params['num_demo'] = 10
+    # Because default number of demo is 50
+    params['num_demo'] = 15
 
     params.update(kwargs)
     config.log_params(params, logger=logger)
